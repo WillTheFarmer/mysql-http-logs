81ea7cef8ce90a80b2e2ebbd6b482a600bbd4b43
WillTheFarmer <farmfreshsoftware@gmail.com>
2026-02-15 07:14:31 -0500

    default mysql CREATE USER name change to fit with multi schemas

--- scripts/mysql_user_and_grants.sql
+++ scripts/mysql_user_and_grants.sql
@@ -1,42 +1,42 @@
--- IF (SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'http_upload' AND host = 'localhost'));
-  CREATE USER 'http_upload'@'localhost' IDENTIFIED BY 'Just4TheData';
+-- IF (SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'logfile_2_mysql' AND host = 'localhost'));
+  CREATE USER 'logfile_2_mysql'@'localhost' IDENTIFIED BY 'Just4TheData';
 -- END IF;  
 
 -- Python module CALLS Stored Procedures for log processing & Stored Procedure for error logging
-GRANT EXECUTE ON PROCEDURE parse_access_apache_combined TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE import_access_apache_combined TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE parse_access_apache_vhost TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE import_access_apache_vhost TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE parse_access_apache_csv2mysql TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE import_access_apache_csv2mysql TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE parse_error_apache_default TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE import_error_apache_default TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE parse_access_nginx_default TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE import_access_nginx_default TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE parse_error_nginx_default TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE import_error_nginx_default TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE normalize_useragent TO `http_upload`@`localhost`;
-GRANT EXECUTE ON PROCEDURE normalize_client TO `http_upload`@`localhost`;
+GRANT EXECUTE ON PROCEDURE parse_access_apache_combined TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE import_access_apache_combined TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE parse_access_apache_vhost TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE import_access_apache_vhost TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE parse_access_apache_csv2mysql TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE import_access_apache_csv2mysql TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE parse_error_apache_default TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE import_error_apache_default TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE parse_access_nginx_default TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE import_access_nginx_default TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE parse_error_nginx_default TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE import_error_nginx_default TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE normalize_useragent TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON PROCEDURE normalize_client TO `logfile_2_mysql`@`localhost`;
 -- Python module CALLS Stored Procedures for error logging
-GRANT EXECUTE ON PROCEDURE messageLoad TO `http_upload`@`localhost`;
+GRANT EXECUTE ON PROCEDURE messageLoad TO `logfile_2_mysql`@`localhost`;
 -- Python module SELECTS Stored Functions for log processing
-GRANT EXECUTE ON FUNCTION importDeviceID TO `http_upload`@`localhost`;
-GRANT EXECUTE ON FUNCTION importClientID TO `http_upload`@`localhost`;
-GRANT EXECUTE ON FUNCTION importLoadID TO `http_upload`@`localhost`;
-GRANT EXECUTE ON FUNCTION importLoadProcessID TO `http_upload`@`localhost`;
-GRANT EXECUTE ON FUNCTION importFileExists TO `http_upload`@`localhost`;
-GRANT EXECUTE ON FUNCTION importFileID TO `http_upload`@`localhost`;
+GRANT EXECUTE ON FUNCTION importDeviceID TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON FUNCTION importClientID TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON FUNCTION importLoadID TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON FUNCTION importLoadProcessID TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON FUNCTION importFileExists TO `logfile_2_mysql`@`localhost`;
+GRANT EXECUTE ON FUNCTION importFileID TO `logfile_2_mysql`@`localhost`;
 -- Python module INSERTS into TABLES executing LOAD DATA LOCAL INFILE for log processing
-GRANT INSERT ON load_access_apache_combined TO `http_upload`@`localhost`;
-GRANT INSERT ON load_access_apache_csv2mysql TO `http_upload`@`localhost`;
-GRANT INSERT ON load_access_apache_vhost TO `http_upload`@`localhost`;
-GRANT INSERT ON load_access_nginx_default TO `http_upload`@`localhost`;
-GRANT INSERT ON load_error_apache_default TO `http_upload`@`localhost`;
-GRANT INSERT ON load_error_apache_csv2mysql TO `http_upload`@`localhost`;
-GRANT INSERT ON load_error_nginx_default TO `http_upload`@`localhost`;
+GRANT INSERT ON load_access_apache_combined TO `logfile_2_mysql`@`localhost`;
+GRANT INSERT ON load_access_apache_csv2mysql TO `logfile_2_mysql`@`localhost`;
+GRANT INSERT ON load_access_apache_vhost TO `logfile_2_mysql`@`localhost`;
+GRANT INSERT ON load_access_nginx_default TO `logfile_2_mysql`@`localhost`;
+GRANT INSERT ON load_error_apache_default TO `logfile_2_mysql`@`localhost`;
+GRANT INSERT ON load_error_apache_csv2mysql TO `logfile_2_mysql`@`localhost`;
+GRANT INSERT ON load_error_nginx_default TO `logfile_2_mysql`@`localhost`;
 -- Python module issues SELECT and UPDATE statements on TABLES due to converting parameters.
 -- Only reason TABLE direct access is number of parameters required for Stored Procedure.
-GRANT SELECT,UPDATE ON access_log_useragent TO `http_upload`@`localhost`;
-GRANT SELECT,UPDATE ON import_process TO `http_upload`@`localhost`;
-GRANT SELECT,UPDATE ON import_load TO `http_upload`@`localhost`;
-GRANT SELECT,UPDATE ON log_client TO `http_upload`@`localhost`;
+GRANT SELECT,UPDATE ON access_log_useragent TO `logfile_2_mysql`@`localhost`;
+GRANT SELECT,UPDATE ON import_process TO `logfile_2_mysql`@`localhost`;
+GRANT SELECT,UPDATE ON import_load TO `logfile_2_mysql`@`localhost`;
+GRANT SELECT,UPDATE ON log_client TO `logfile_2_mysql`@`localhost`;
